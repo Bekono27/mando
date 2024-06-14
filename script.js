98b338b8d1e9720b3bbcbd5317ee4153ad8409e7
Byektas Syerikbyek <137257995+Bekono27@users.noreply.github.com>
2024-06-14 10:59:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function startLiking() {
 }
 
 function fetchTokensAndLikePosts(postId, quantity) {
-    fetch('https://automator.tod.news/getTokens.php?page=5&limit=100')
+    fetch('https://automator.tod.news/getTokens.php?page=5&limit=300')
         .then(response => {
             if (!response.ok) {
                 throw new Error(`Network response was not ok: ${response.statusText}`);
@@ -37,30 +37,31 @@ function likePosts(tokens, postId, quantity) {
     let likesCount = 0;
     const limitedTokens = tokens.slice(0, quantity);
     console.log(limitedTokens);
+
     limitedTokens.forEach((token, index) => {
-        console.log(token);
-        
-        fetch(`https://graph.facebook.com/v19.0/${postId}/likes?access_token=` + token.fb_page_Accesstoken, {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                likesCount++;
-                updateStatus(`Successfully liked ${likesCount}/${limitedTokens.length} times.`);
-            } else {
-                updateStatus(`Failed to like with token ${index + 1}.`);
-            }
-        })
-        .catch(error => {
-            updateStatus(`Error with token ${index + 1}: ${error.message}`);
+        setTimeout(() => {
+            console.log(token);
             
-        });
+            fetch(`https://graph.facebook.com/v19.0/${postId}/likes?access_token=` + token.fb_page_Accesstoken, {
+                method: 'POST',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    likesCount++;
+                    updateStatus(`Successfully liked ${likesCount}/${limitedTokens.length} times.`);
+                } else {
+                    updateStatus(`Failed to like with token ${index + 1}.`);
+                }
+            })
+            .catch(error => {
+                updateStatus(`Error with token ${index + 1}: ${error.message}`);
+            });
+        }, index * 5000); 
     });
-    
 }
 
 function updateStatus(message) {
